cdd18c52c555b41cf6c7df41ba19da4eacf19f65
22-2025082-afk <22-2025082@edu.city.izumi.osaka.jp>
2026-07-05 17:30:25 +0900

    Create abilities.js

---
+++ data/abilities.js
@@ -0,0 +1,55 @@
+// data/abilities.js
+// Pokémon Champions ダメージ計算機
+// 特性データ
+
+const ABILITIES = [
+
+    {
+        id: "overgrow",
+        name: "しんりょく",
+        description: "HPが1/3以下のとき、くさタイプの技の威力が上がる。"
+    },
+
+    {
+        id: "blaze",
+        name: "もうか",
+        description: "HPが1/3以下のとき、ほのおタイプの技の威力が上がる。"
+    },
+
+    {
+        id: "torrent",
+        name: "げきりゅう",
+        description: "HPが1/3以下のとき、みずタイプの技の威力が上がる。"
+    },
+
+    {
+        id: "static",
+        name: "せいでんき",
+        description: "直接攻撃を受けると、相手をまひ状態にすることがある。"
+    },
+
+    {
+        id: "lightning-rod",
+        name: "ひらいしん",
+        description: "でんきタイプの技を引き寄せ、特攻が上がる。"
+    },
+
+    {
+        id: "solar-power",
+        name: "サンパワー",
+        description: "晴れのとき特攻が上がるが、毎ターンダメージを受ける。"
+    },
+
+    {
+        id: "chlorophyll",
+        name: "ようりょくそ",
+        description: "晴れのとき素早さが2倍になる。"
+    },
+
+    {
+        id: "rain-dish",
+        name: "あめうけざら",
+        description: "雨のとき毎ターンHPが回復する。"
+    }
+
+];
