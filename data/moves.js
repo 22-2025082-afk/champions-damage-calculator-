a66abb15ad02f6c6912ddae5af79dcb16db20594
22-2025082-afk <22-2025082@edu.city.izumi.osaka.jp>
2026-07-05 15:40:23 +0900

    Create moves.js

---
+++ data/moves.js
@@ -0,0 +1,107 @@
+// data/moves.js
+// 技データ
+// タイプ・カテゴリーは内部処理用に英語で管理する
+
+const MOVES = [
+
+    {
+        id: 33,
+        name: "たいあたり",
+        type: "normal",
+        category: "physical",
+        power: 40,
+        accuracy: 100,
+        pp: 35
+    },
+
+    {
+        id: 53,
+        name: "かえんほうしゃ",
+        type: "fire",
+        category: "special",
+        power: 90,
+        accuracy: 100,
+        pp: 15
+    },
+
+    {
+        id: 57,
+        name: "なみのり",
+        type: "water",
+        category: "special",
+        power: 90,
+        accuracy: 100,
+        pp: 15
+    },
+
+    {
+        id: 85,
+        name: "10まんボルト",
+        type: "electric",
+        category: "special",
+        power: 90,
+        accuracy: 100,
+        pp: 15
+    },
+
+    {
+        id: 89,
+        name: "じしん",
+        type: "ground",
+        category: "physical",
+        power: 100,
+        accuracy: 100,
+        pp: 10
+    },
+
+    {
+        id: 126,
+        name: "だいもんじ",
+        type: "fire",
+        category: "special",
+        power: 110,
+        accuracy: 85,
+        pp: 5
+    },
+
+    {
+        id: 200,
+        name: "げきりん",
+        type: "dragon",
+        category: "physical",
+        power: 120,
+        accuracy: 100,
+        pp: 10
+    },
+
+    {
+        id: 247,
+        name: "シャドーボール",
+        type: "ghost",
+        category: "special",
+        power: 80,
+        accuracy: 100,
+        pp: 15
+    },
+
+    {
+        id: 399,
+        name: "インファイト",
+        type: "fighting",
+        category: "physical",
+        power: 120,
+        accuracy: 100,
+        pp: 5
+    },
+
+    {
+        id: 406,
+        name: "りゅうせいぐん",
+        type: "dragon",
+        category: "special",
+        power: 130,
+        accuracy: 90,
+        pp: 5
+    }
+
+];
