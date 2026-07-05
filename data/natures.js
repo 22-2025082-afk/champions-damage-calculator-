e63769b6d0fd22989da8f172c9b6539f24ce40fb
22-2025082-afk <22-2025082@edu.city.izumi.osaka.jp>
2026-07-05 17:26:10 +0900

    Create natures.js

---
+++ data/natures.js
@@ -0,0 +1,182 @@
+// data/natures.js
+// Pokémon Champions ダメージ計算機
+// 性格データ
+
+const NATURES = [
+
+    {
+        id: "hardy",
+        name: "がんばりや",
+        up: null,
+        down: null
+    },
+
+    {
+        id: "lonely",
+        name: "さみしがり",
+        up: "atk",
+        down: "def"
+    },
+
+    {
+        id: "brave",
+        name: "ゆうかん",
+        up: "atk",
+        down: "spe"
+    },
+
+    {
+        id: "adamant",
+        name: "いじっぱり",
+        up: "atk",
+        down: "spa"
+    },
+
+    {
+        id: "naughty",
+        name: "やんちゃ",
+        up: "atk",
+        down: "spd"
+    },
+
+    {
+        id: "bold",
+        name: "ずぶとい",
+        up: "def",
+        down: "atk"
+    },
+
+    {
+        id: "docile",
+        name: "すなお",
+        up: null,
+        down: null
+    },
+
+    {
+        id: "relaxed",
+        name: "のんき",
+        up: "def",
+        down: "spe"
+    },
+
+    {
+        id: "impish",
+        name: "わんぱく",
+        up: "def",
+        down: "spa"
+    },
+
+    {
+        id: "lax",
+        name: "のうてんき",
+        up: "def",
+        down: "spd"
+    },
+
+    {
+        id: "timid",
+        name: "おくびょう",
+        up: "spe",
+        down: "atk"
+    },
+
+    {
+        id: "hasty",
+        name: "せっかち",
+        up: "spe",
+        down: "def"
+    },
+
+    {
+        id: "serious",
+        name: "まじめ",
+        up: null,
+        down: null
+    },
+
+    {
+        id: "jolly",
+        name: "ようき",
+        up: "spe",
+        down: "spa"
+    },
+
+    {
+        id: "naive",
+        name: "むじゃき",
+        up: "spe",
+        down: "spd"
+    },
+
+    {
+        id: "modest",
+        name: "ひかえめ",
+        up: "spa",
+        down: "atk"
+    },
+
+    {
+        id: "mild",
+        name: "おっとり",
+        up: "spa",
+        down: "def"
+    },
+
+    {
+        id: "quiet",
+        name: "れいせい",
+        up: "spa",
+        down: "spe"
+    },
+
+    {
+        id: "bashful",
+        name: "てれや",
+        up: null,
+        down: null
+    },
+
+    {
+        id: "rash",
+        name: "うっかりや",
+        up: "spa",
+        down: "spd"
+    },
+
+    {
+        id: "calm",
+        name: "おだやか",
+        up: "spd",
+        down: "atk"
+    },
+
+    {
+        id: "gentle",
+        name: "おとなしい",
+        up: "spd",
+        down: "def"
+    },
+
+    {
+        id: "sassy",
+        name: "なまいき",
+        up: "spd",
+        down: "spe"
+    },
+
+    {
+        id: "careful",
+        name: "しんちょう",
+        up: "spd",
+        down: "spa"
+    },
+
+    {
+        id: "quirky",
+        name: "きまぐれ",
+        up: null,
+        down: null
+    }
+
+];
